8a6feb70e44752cc06f017deed1e5df9a1dda367
scapegoatee <43176120+scapegoatee@users.noreply.github.com>
2018-11-27 16:44:47 +0530

    Update fibonacci.rb

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,9 +1,4 @@
 # Generate Fibonacci Series upto n.
-# 0 + 1 = 1, 1 + 1 = 2, 2 + 1 = 3, 3 + 2 = 5, 5 + 3 = 8,...
-# x   y   c  c   x   
-# when x = 2, y = 3, z = 5
-# when
-
 
 def fibonacci(n)
   series = [0, 1]
